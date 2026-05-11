a70127d4fd80fb6a64747ca42bfad57b50f33d77
msepac <matteo.sepac@atb.com>
2026-05-11 14:39:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ CREDIBLE_KEYWORDS = [
     "Associated Press", "AP", "Canadian Press", "GlobeNewswire", "Globe Newswire", 
     "CNW Group", "PR Newswire", "Business Wire", "BusinessWire", "Accesswire", 
     "Newsfile", "Marketwired", "Morningstar", "Barchart", "Seeking Alpha", 
-    "MarketWatch", "Newswire", "TMX", "Press Release", "Government of Canada", 
+    "MarketWatch", "Newswire", "TMX", "Press Release", "Government of Canada", "RENX", "The National Post", 
 ]
 
 def is_credible_source(source_name, company_name=""):
